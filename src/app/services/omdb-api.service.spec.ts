184b144fabe618b5eec1b5531a2068d6705db652
Sean Dowdall <seanied1234567@gmail.com>
2022-03-17 23:54:01 +0000

    Page 6 done - Worksheet7

---
+++ src/app/services/omdb-api.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { OmdbApiService } from './omdb-api.service';
+
+describe('OmdbApiService', () => {
+  let service: OmdbApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(OmdbApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/services/omdb-api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { IOMDBResponse } from '../omdbresponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OmdbApiService {
+
+  private _siteURL="https://www.omdbapi.com/?i=tt3896198"
+  private _key="?apikey=bf7ad2c7"
+
+  constructor(private _http:HttpClient) { }
+
+  getMovieData(movieName): Observable<IOMDBResponse> {
+    return this._http.get<IOMDBResponse>(this._siteURL + this._key + movieName)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
+  }
+
+  private handleError(err:HttpErrorResponse) {
+    console.log('OmdbApiService' + err.message);
+    return throwError("error : " + err.message);
+  }
+
+  
+}
